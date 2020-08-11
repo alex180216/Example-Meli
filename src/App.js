b30b2eebebd73e181dd8f0ce732d758227d2bd8f
Alejandra <marietsy.911.mm@gmail.com>
2020-08-11 19:27:11 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import Saludo from './Saludo'
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-meli.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text" className="new-task" placeholder="Nunca dejes de buscar"/>
+      </div>
+      <Saludo/>
     </div>
   );
 }
