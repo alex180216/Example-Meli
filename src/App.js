3ba2d6f49eee89f638d2813e8777ae3deb43837f
Alejandra <marietsy.911.mm@gmail.com>
2020-08-13 18:11:18 -0300

    recibir 4 items de la api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Resultados from './Resultados';
 import React from 'react';
 import './App.css';
 
+var cosas = [];
 class App extends React.Component {
+
+  
 
   constructor(props){
     super(props);
@@ -13,6 +16,27 @@ class App extends React.Component {
         {id:'0', thumbnail:'http://http2.mlstatic.com/D_608380-MLA32167208403_092019-O.jpg', title:'Mesa Mesita De Luz Vintage Retro Nordica Pino Con Cajones' , price:'2450', address:'Capital'}
       ]
     };
+  }
+
+  componentDidMount(){
+    fetch('https://api.mercadolibre.com/sites/MLA/search?q=Motorola%20G6')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      /* console.log(response.results); */
+
+      for( let i=0; i < 4; i++){
+        console.log(response.results[i]);
+        cosas.push(response.results[i]);
+      }
+      /* response.results.forEach(element => {
+        console.log(element.title);
+        
+      }); */
+    })
+
+    console.log(cosas);
   }
 
   render(){
@@ -26,6 +50,7 @@ class App extends React.Component {
       
     );
   }
+
   
 }
 
